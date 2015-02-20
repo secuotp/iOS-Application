061420b5edee216861cd5a0e2e8c893e4e9c5fe6
Panasan Sinroungrong <zenology@Panasans-MacBook-Pro.local>
2015-02-20 17:34:22 +0700

    Create 3 new View Controller

---
+++ SecuOTP Application/AppSettingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AppSettingViewController.swift
+//  SecuOTP Application
+//
+//  Created by Panasan Sinroungrong on 2/14/15.
+//  Copyright (c) 2015 SecuOTP. All rights reserved.
+//
+
+import UIKit
+
+class AppSettingViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setting Title Bar Name
+        self.navigationItem.title = "Setting"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
