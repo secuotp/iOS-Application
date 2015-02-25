f48defe6e11b3d0cf86f4afd9dd7e0e4e705ffd7
Panasan Sinroungrong <zenology@Panasans-MacBook-Pro.local>
2015-02-26 04:28:08 +0700

    Add Create Password Function (Completed) & Reposition Storyboard

--- SecuOTP Application/ConfirmPasswordViewController.swift
+++ SecuOTP Application/ConfirmPasswordViewController.swift
@@ -1,34 +1,34 @@
 //
-//  CreatePasswordViewController.swift
+//  ConfirmPasswordViewController.swift
 //  SecuOTP Application
 //
-//  Created by Panasan Sinroungrong on 2/25/15.
+//  Created by Panasan Sinroungrong on 2/26/15.
 //  Copyright (c) 2015 SecuOTP. All rights reserved.
 //
 
 import UIKit
 
-class CreatePasswordViewController: UIViewController {
+class ConfirmPasswordViewController: UIViewController {
 
-    // Outlet
     @IBOutlet weak var dot1: UILabel!
     @IBOutlet weak var dot2: UILabel!
     @IBOutlet weak var dot3: UILabel!
     @IBOutlet weak var dot4: UILabel!
     
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var textBanner: UILabel!
     
+    // Constraint Only
+    @IBOutlet weak var textLabel: UILabel!
+   
     // Parameter
-    var pin : NSString?
-    
+    var password: NSString?
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let topDotViewConstraint : NSLayoutConstraint = NSLayoutConstraint(item: textBanner, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: self.view.bounds.height * 0.2)
-        
-        self.view.addConstraint(topDotViewConstraint)
         // Do any additional setup after loading the view.
+        let topLabelViewConstraint : NSLayoutConstraint = NSLayoutConstraint(item: textLabel, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: self.view.bounds.height * 0.2)
+        
+        self.view.addConstraint(topLabelViewConstraint)
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,34 +37,9 @@ class CreatePasswordViewController: UIViewController {
     }
     
     override func viewDidAppear(animated: Bool) {
-        // Open Keyboard
         textField.becomeFirstResponder()
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "CreatePasswordToConfirmPassword" {
-            let confirmViewController: ConfirmPasswordViewController = segue.destinationViewController as ConfirmPasswordViewController
-            confirmViewController.password = pin
-            
-            pin = nil
-            textField.text = ""
-            
-            dot1.text = "-"
-            dot2.text = "-"
-            dot3.text = "-"
-            dot4.text = "-"
-        }
-    }
-    
-    // When Edit Changed
     @IBAction func whenKeyboardTap(sender: AnyObject) {
         if countElements(textField.text) == 0 {
             dot1.text = "-"
@@ -97,8 +72,16 @@ class CreatePasswordViewController: UIViewController {
                 title: "Yes",
                 style: UIAlertActionStyle.Default,
                 handler: { action in
-                    self.pin = self.textField.text!
-                    self.performSegueWithIdentifier("CreatePasswordToConfirmPassword", sender: self)
+                    if self.password! == self.textField.text {
+                        let config:ConfigEntity = ConfigEntity()
+                        config.save(self.password!)
+                        self.performSegueWithIdentifier("ConfirmPasswordToLogin", sender: self)
+                    } else {
+                        let errorAlert: UIAlertView = UIAlertView(title: "Create PIN Failed", message: "Your entered PIN is mismatched\nCreated Again", delegate: nil, cancelButtonTitle: "OK")
+                        
+                        errorAlert.show()
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    }
                 }
             )
             let no : UIAlertAction = UIAlertAction(
@@ -119,4 +102,14 @@ class CreatePasswordViewController: UIViewController {
             self.presentViewController(alert, animated: true, completion: nil)
         }
     }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
